a32835202d15472e95d3d5218a7557c4020d80c2
shixi2ng <shixi2ng@gmail.com>
2024-05-21 22:11:06 +0800

    Landsat toolbox Update 2024/05/15

---
+++ TEMP2.py
@@ -0,0 +1,5 @@
+from RSDatacube.RSdc import Phemetric_dc
+
+
+Pheme2022 = Phemetric_dc(f'G:\\A_veg\\S2_all\\Sentinel2_L2A_Output\\Sentinel2_MYZR_FP_2020_datacube\\OSAVI_20m_noninun_curfit_datacube\\MYZR_FP_2020_Phemetric_datacube\\2022\\')
+Pheme2022.calculate_phemetrics(['SOS', 'EOS', 'trough_vi', 'peak_vi', 'peak_doy', 'GR', 'DR', 'DR2'])
